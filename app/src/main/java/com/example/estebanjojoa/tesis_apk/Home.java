95d6aadd3b5471669b5d59cfbefe8f8ed5b9649f
Esteban_Dario_Jojoa_Rosero <estebandariojojoa9@gmail.com>
2019-01-17 16:02:07 -0500

    Initial commit

---
+++ app/src/main/java/com/example/estebanjojoa/tesis_apk/Home.java
@@ -0,0 +1,55 @@
+package com.example.estebanjojoa.tesis_apk;
+
+import android.app.FragmentTransaction;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class Home extends AppCompatActivity {
+
+    BottomNavigationView btn_navigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        btn_navigation = (BottomNavigationView) findViewById(R.id.botton_navigation);
+        btn_navigation.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.framelayout, new inicio()).commit();
+    }
+
+   private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+               switch (item.getItemId()){
+                       case R.id.Item_home:
+                           selectedFragment = new inicio();
+                           break;
+
+                        case R.id.Item_medicion:
+                            selectedFragment = new mediciones();
+                            break;
+
+                        case R.id.Item_graficas:
+                            selectedFragment = new graficas();
+                            break;
+
+                        case R.id.Item_medicamentos:
+                            selectedFragment = new medicamentos();
+                             break;
+
+                        case R.id.Item_control:
+                            selectedFragment = new factores();
+                            break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout, selectedFragment).commit();
+                return true;
+            }
+
+        };
+}
