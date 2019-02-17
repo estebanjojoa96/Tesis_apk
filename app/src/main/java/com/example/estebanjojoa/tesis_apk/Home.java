6af43fa7a9bc55c87dfa95cfdb0526dbc5feba2c
Esteban_Dario_Jojoa_Rosero <estebandariojojoa9@gmail.com>
2019-02-17 18:00:55 -0500

    segundo commit

--- app/src/main/java/com/example/estebanjojoa/tesis_apk/Home.java
+++ app/src/main/java/com/example/estebanjojoa/tesis_apk/Home.java
@@ -8,9 +8,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class Home extends AppCompatActivity {
 
     BottomNavigationView btn_navigation;
+    GraphView graph = (GraphView) findViewById(R.id.graph);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +25,9 @@ public class Home extends AppCompatActivity {
         btn_navigation = (BottomNavigationView) findViewById(R.id.botton_navigation);
         btn_navigation.setOnNavigationItemSelectedListener(navListener);
         getSupportFragmentManager().beginTransaction().replace(R.id.framelayout, new inicio()).commit();
+
     }
+
 
    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
--- app/src/main/java/com/example/estebanjojoa/tesis_apk/graficas.java
+++ app/src/main/java/com/example/estebanjojoa/tesis_apk/graficas.java
@@ -7,6 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,11 +23,18 @@ public class graficas extends Fragment {
     }
 
 
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_graficas, container, false);
+
+
+
+
     }
 
 }
